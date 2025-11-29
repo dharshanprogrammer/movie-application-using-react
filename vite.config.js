07bdb8a63125501f7ba85d33aa35efaa62c5cc44
dharshanprogrammer <dharshanprogrammer@gmail.com>
2025-11-29 14:11:23 +0530

    another fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: import.meta.env.VITE_BASE_PATH || "/movie-application-using-react",
+  base:process.env.VITE_BASE_PATH || "/movie-application-using-react",
   server: {
     sourcemapIgnoreList() { return true } // suppress map lookups in dev
   },
